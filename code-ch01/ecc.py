37ee7b0bf9ab9045a890a800a97ad9266af4b678
Bryan Ramos <bryan@ramos.codes>
2024-02-08 16:43:59 -0500

    Ch01 Done

--- code-ch01/ecc.py
+++ code-ch01/ecc.py
@@ -64,7 +64,7 @@ class FieldElement:
         # this means:
         # 1/n == pow(n, p-2, p)
         # We return an element of the same class
-        num = self.num * pow(other.num, self.prime - 2, self.prime) % self.prime
+        num = (self.num * (pow(other.num, self.prime - 2, self.prime)) % self.prime)
         return self.__class__(num, self.prime)
 
 class FieldElementTest(TestCase):
